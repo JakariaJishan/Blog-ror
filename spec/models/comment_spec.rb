243a0bcd2e9c8495f4cd2c093a1b7dae5a008eb8
JakariaJishan <jakariajishan987@gmail.com>
2023-05-13 04:04:34 +0600

    Fix rubocop error

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-    let(:user) { User.new(name: 'Tom', posts_counter: 0) }
-    let(:post) { Post.create(author: user, title: 'Hello', likes_counter: 0, comments_counter: 0) }
-    
-    it 'update_like_counter method should increment user likes counter by one' do
-      Comment.create(text: 'comment1', author: user, post:post)
-      Comment.create(text: 'comment2', author: user, post:post)
-      expect(post.comments_counter).to equal(2)
-    end
-   
+  let(:user) { User.new(name: 'Tom', posts_counter: 0) }
+  let(:post) { Post.create(author: user, title: 'Hello', likes_counter: 0, comments_counter: 0) }
+
+  it 'update_like_counter method should increment user likes counter by one' do
+    Comment.create(text: 'comment1', author: user, post:)
+    Comment.create(text: 'comment2', author: user, post:)
+    expect(post.comments_counter).to equal(2)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,28 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 2) }
 
-    subject {User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 2)}
+  before { subject.save }
 
-    before {subject.save}
-  
-    it('user name should be valid') do
-      subject.name = nil
-      expect(subject).to_not be_valid
-    end
-  
-    it('return false if posts_counter is not a positive integer') do
-        subject.posts_counter = -5
-        expect(subject).to_not be_valid
-    end
-  
-    let (:user) {User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0)}
-  
-    it('last_three_post method should return last three posts') do
-      Post.create(author: user, title: 'title1', likes_counter: 0, comments_counter: 0)
-      Post.create(author: user, title: 'title2', likes_counter: 0, comments_counter: 0)
-      Post.create(author: user, title: 'title3', likes_counter: 0, comments_counter: 0)
-      Post.create(author: user, title: 'title4', likes_counter: 0, comments_counter: 0)
-      expect(user.last_three_post.length).to equal(3)
-    end
+  it('user name should be valid') do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it('return false if posts_counter is not a positive integer') do
+    subject.posts_counter = -5
+    expect(subject).to_not be_valid
+  end
+
+  let(:user) { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0) }
+
+  it('last_three_post method should return last three posts') do
+    Post.create(author: user, title: 'title1', likes_counter: 0, comments_counter: 0)
+    Post.create(author: user, title: 'title2', likes_counter: 0, comments_counter: 0)
+    Post.create(author: user, title: 'title3', likes_counter: 0, comments_counter: 0)
+    Post.create(author: user, title: 'title4', likes_counter: 0, comments_counter: 0)
+    expect(user.last_three_post.length).to equal(3)
+  end
 end
