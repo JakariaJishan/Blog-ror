af844fd94fe384e95ae593a0bc4a81c4ff4cfbfb
JakariaJishan <jakariajishan987@gmail.com>
2023-05-12 15:49:33 +0600

    Fix rubocop errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
-belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
   belongs_to :post
-  
+
   after_save :update_comments_counter
 
   def update_comments_counter
