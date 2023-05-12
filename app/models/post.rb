cd489e00bf8be12a8eaf791c37d1d6197c2ab960
JakariaJishan <jakariajishan987@gmail.com>
2023-05-13 02:48:57 +0600

    Add validations for user and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
+
+  validates :title, presence: true, lenght: {maximum: 250}
+  validates :comments_counter, numericality: {greater_then_or_equal_to: 0, only_integer: true}
+  validates :likes_counter, numericality: {greater_then_or_equal_to: 0, only_integer: true}
+
+
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
