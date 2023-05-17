0bb32ad3d8584a92324a95435fa317cc6e91af25
JakariaJishan <jakariajishan987@gmail.com>
2023-05-18 05:08:08 +0600

    Create viewes for users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.find_by(author_id: params[:user_id])
+  end
 
   def show
     @post_id = params[:id]
-    @user_id = params[:user_id]
   end
 end
