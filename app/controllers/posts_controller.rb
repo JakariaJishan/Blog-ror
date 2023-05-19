64dbfb08ecf74551e12f35c2b33c5c9f2b3ce188
JakariaJishan <jakariajishan987@gmail.com>
2023-05-20 01:59:47 +0600

    Create form and method for adding post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,26 @@ class PostsController < ApplicationController
     @posts = Post.where(author_id: params[:user_id])
     @user = User.find(params[:user_id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+   @post = Post.new(post_params)
+   @post.author = current_user
+    puts :post
+    if @post.save
+      redirect_to "/users/#{current_user.id}/posts"
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text).merge(author_id: current_user.id, comments_counter: 0, likes_counter: 0)
+  end
+
 end
