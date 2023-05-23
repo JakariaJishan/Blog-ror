09580a465dcebab77589e01212ce3dd1f33951f5
Balikuddembe <balixemma@gmail.com>
2023-05-23 07:57:43 +0300

    Fix linter errors

--- spec/system/post/post_show_spec.rb
+++ spec/system/post/post_show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Post show page', type: :system do
- 
+
   before(:each) do
     @user = User.create(name: 'Jishan', photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=374&q=80', bio: 'Teacher from Mexico.', posts_counter: 2) 
 
@@ -39,7 +39,7 @@ RSpec.describe 'Post show page', type: :system do
     visit user_post_path(@user.id, @post.id)
     expect(page).to have_content('Jishan')
   end
-  
+
   it 'should show click on a post, it redirects me to that post\'s show page' do
     visit user_posts_path(@user.id)
     click_link 'this is text'
