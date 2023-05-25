7c2df91266378f547439652d4bed1f0d0b13a21f
JakariaJishan <jakariajishan987@gmail.com>
2023-05-25 23:49:29 +0600

    Add styling to delete button

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,23 +4,6 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # user ||= User.new
-    # if user.role == 'admin'
-    #   can :manage, :all
-    # else
-    #   can :manage, Post, author_id: user.id
-    #   # can :manage, Comment, author_id: user.id
-    #   can :read, :all
-    # # can :destroy, Post if user.role == 'Admin'
-    # end
-
-   #  user ||= User.new # guest user (not logged in)
-   #  case user.role
-   #     when "admin" #-> use double quotes for evaluating strings
-   #        can :manage, :all
-   #     when "user"
-   #        can :manage, [Post, Comment], author_id: user.id
-   #     end
 
    return unless user.present?
     can :read, :all
